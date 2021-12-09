fbbd27aa286a1ba75f76916d2199097aae7e4d43
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-12-09 03:46:35 -0500

    added functionality to be able to log items from a starting timestamp

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,21 @@ use warp::Filter;
 // Dependencies for hash string converter
 use sp_core::sr25519::Public;
 
-// TEST, should remove after test
-#[derive(Deserialize, Serialize)]
-struct Employee {
-    name: String,
-    rate: u32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Sample {
-  person_id: i32,
-  person_name: String
-}
+use chrono::{DateTime, Utc, Duration};
+use chrono::prelude::*;
 
 #[derive(Encode, Decode, Clone, Default, Eq, PartialEq, Debug, Deserialize, Serialize)]
 pub struct AuditLog {
-    // Change the timestamp to a timestamp type handled by Substrate itself
     // Reporter determines which system sent the log
     content: String,
     reporter: Public,
+}
+
+#[derive(Encode, Decode, Clone, Default, Eq, PartialEq, Debug, Deserialize, Serialize)]
+pub struct AuditLogSummary {
+    // Reporter determines which system sent the log
+    filename: String,
+    contents: Vec<AuditLog>,
 }
 
 #[tokio::main]
@@ -50,9 +46,13 @@ async fn main() {
     let get_balances = warp::path!("get-balances")
     .map(|| get_balances());
 
-    let get_logs_of_filename = warp::path!("get-logs-of-file" / String)
-    .map(|log_filename| get_logs_of_file(log_filename));
+    let get_logs_of_file_from_timestamp = warp::path!("get-logs-of-file" / String / String)
+    .map(|log_filename, log_timestamp| get_file_logs_from_timestamp(log_filename, log_timestamp));
 
+    /*
+    let get_file_logs_from_timestamp = warp::path!("get-logs" / String / String)
+    .map(|log_filename| get_file_logs_from_timestamp(log_filename));
+    */
     let add_log = warp::path!("add-log" / String / String)
         .map(|log_filename, log_content| 
           add_log(log_filename, log_content)
@@ -61,7 +61,7 @@ async fn main() {
     let routes = warp::get().and(
       ping_chain
           .or(get_balances)
-          .or(get_logs_of_filename)
+          .or(get_logs_of_file_from_timestamp)
           .or(add_log),
   );
 
@@ -124,6 +124,51 @@ fn get_logs_of_file(log_filename: String) -> std::string::String {
   format!("[+] log items are {:?}", result)
 }
 
+// TODO: The output in the Response is quite dirty, need to clean it up
+fn get_file_logs_from_timestamp(log_filename: String, log_timestamp: String) -> std::string::String {
+  let result: Vec<AuditLog> = collect_file_logs_from_timestamp_range(&log_filename, &log_timestamp, &Utc::now().to_rfc3339());
+  let result_summary = AuditLogSummary {
+    filename: log_filename,
+    contents: result
+  };
+  format!("{:?}", serde_json::to_string(&result_summary).unwrap())
+}
+
+// NOTE: This currently has O(n) so it needs refactoring for performance at some point
+// NOTE: This function can be in a utility class
+// TODO: Must deal with nanoseconds items
+fn collect_file_logs_from_timestamp_range(log_filename: &String, log_timestamp_begin: &String, log_timestamp_end: &String) -> Vec<AuditLog> {
+
+  let str_timestmp_begin = DateTime::parse_from_rfc3339(&log_timestamp_begin[..]).unwrap();
+  let str_timestmp_end = DateTime::parse_from_rfc3339(&log_timestamp_end[..]).unwrap();
+  let timestamp_diff: i64 = str_timestmp_end.signed_duration_since(str_timestmp_begin).num_seconds();
+
+  let mut time_seconds = 0;
+  let mut audit_logs: Vec<AuditLog> = Vec::new();
+
+  // TODO: Should only push Audit Logs that have contents
+  while time_seconds < timestamp_diff {
+    time_seconds = time_seconds + 1;
+    let adjusted_log_timestamp = str_timestmp_begin + Duration::seconds(time_seconds);
+    println!("date_time of retrieval {}", adjusted_log_timestamp);
+    audit_logs.push(retrieve_log_from(&log_filename, &adjusted_log_timestamp.to_rfc3339().to_string()));
+  }
+
+  audit_logs
+}
+
+// NOTE: This function can be in a utility class
+fn retrieve_log_from(log_filename: &String, log_timestamp: &String) -> AuditLog {
+  let url = "ws://127.0.0.1:9944";
+  
+  let client = WsRpcClient::new(url);
+  let api = Api::<sr25519::Pair, _>::new(client).unwrap();
+  api.get_storage_double_map("Auditor", "AuditLogStorage", log_filename.to_string().into_bytes(), log_timestamp.to_string().into_bytes(), None)
+        .unwrap()
+        .or_else(|| Some(AuditLog::default()))
+        .unwrap()
+}
+
 fn add_log(log_filename: String, log_content: String) -> std::string::String {
 
   let url = "ws://127.0.0.1:9944";
@@ -131,6 +176,9 @@ fn add_log(log_filename: String, log_content: String) -> std::string::String {
   let client = WsRpcClient::new(url);
   let from = AccountKeyring::Alice.pair();
   let api = Api::new(client).map(|api| api.set_signer(from)).unwrap();
+
+  let now: DateTime<Utc> = Utc::now().round_subsecs(0);
+  println!("date_time of added_log {}", &now.to_rfc3339().to_string());
 
   // NOTE: save_audit_log exists in Auditor pallet thats why this works
   #[allow(clippy::redundant_clone)]
@@ -140,7 +188,7 @@ fn add_log(log_filename: String, log_content: String) -> std::string::String {
     "save_audit_log",
     log_filename.to_string().into_bytes(),
     log_content.to_string().into_bytes(),
-    "test_timestamp3".to_string().into_bytes()
+    now.to_rfc3339().to_string().into_bytes()
   );
 
   

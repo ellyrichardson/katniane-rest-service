762c7e571541275d29a33b4d38a21589c741c044
Elly Richardson <elly.richardson18@gmail.com>
2021-12-14 23:07:43 -0600

    update AuditLog model to match the substrate chain, and updated the timezone logic code for saving audit logs

--- src/endpoint_handlers/models.rs
+++ src/endpoint_handlers/models.rs
@@ -6,7 +6,9 @@ use sp_core::sr25519::Public;
 #[derive(Encode, Decode, Clone, Default, Eq, PartialEq, Debug, Deserialize, Serialize)]
 pub struct AuditLog {
     // Reporter determines which system sent the log
+    pub title: String,
     pub content: String,
+    pub timestamp: String,
     pub reporter: Public,
 }
 
@@ -14,11 +16,13 @@ pub struct AuditLog {
 pub struct AuditLogSummary {
     // Reporter determines which system sent the log
     pub filename: String,
+    pub date: String,
     pub contents: Vec<AuditLog>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct IncomingAuditLog {
     pub filename: String,
+    pub title: String,
     pub content: String,
 }
